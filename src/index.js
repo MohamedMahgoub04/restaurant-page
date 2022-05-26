da934b77d53ae1f624f8b56ed05606daa2687de4
Mohamed <M.Mahgoub04@gmail.com>
2022-05-26 19:21:00 +0400

    Basic layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-alert('Hey')
+document.addEventListener('DOMContentLoaded', () => {
+ 
+ 
+ const homeBtn = document.querySelector('#home')
+ const menuBtn = document.querySelector('#menu')
+ const contactBtn = document.querySelector('#contact')
+
+ homeBtn.onclick = () => {
+  const home = document.querySelector('#main')
+  const menu = document.querySelector('#main1')
+  const contact = document.querySelector('#main2')
+  home.style.display = 'flex'
+  menu.style.display = 'none'
+  contact.style.display = 'none'
+ }
+ menuBtn.onclick = () => {
+  const home = document.querySelector('#main')
+  const menu = document.querySelector('#main1')
+  const contact = document.querySelector('#main2')
+  home.style.display = 'none'
+  menu.style.display = 'flex'
+  contact.style.display = 'none'
+ }
+ contactBtn.onclick = () => {
+  const home = document.querySelector('#main')
+  const menu = document.querySelector('#main1')
+  const contact = document.querySelector('#main2')
+  home.style.display = 'none'
+  menu.style.display = 'none'
+  contact.style.display = 'flex'
+ }
+
+})
